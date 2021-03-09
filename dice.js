dccb69892e553f07c9387f1c78ef4b85508285e3
Ian Turner <ianturner88@gmail.com>
2021-03-08 19:41:11 -0500

    random numbers between 1 & 6 generated

--- dice.js
+++ dice.js
@@ -1,7 +1,10 @@
 function dice() {
+  // generate random numbers for the dice
+
   let counter = 0;
 
   while (counter < 100) {
+    // creates a nice visual effect of the dice numbers changing
     let test = Math.floor(Math.random() * 6) + 1;
     counter++;
 
