d073f00ed4bf9b55ad1012655a380c8019ff8d2f
Ian Turner <ianturner88@gmail.com>
2021-03-17 20:04:32 -0400

    bedtime

--- functionalities/dice.js
+++ functionalities/dice.js
@@ -1,11 +1,51 @@
-function dice() {
-  // generate random numbers for the dice
+export default class Dice {
+  // the dice used to generate the random numbers 1 through 6
 
-  let counter = 0;
+  constructor() {
+    // initialize all numbers to zero
+    this.publicNumber1 = 0;
+    this.redNumber1 = 0;
+    this.yellowNumber1 = 0;
+    this.greenNumber1 = 0;
+    this.blueNumber1 = 0;
+    this.publicNumber2 = 0;
+    this.redNumber2 = 0;
+    this.yellowNumber2 = 0;
+    this.greenNumber2 = 0;
+    this.blueNumber2 = 0;
+  }
 
-  while (counter < 100) {
-    // creates a nice visual effect of the dice numbers changing
-    let test = Math.floor(Math.random() * 6) + 1;
-    counter++;
+  rollDice() {
+    // initiated by user
+    // all numbers are given a new number between 1 & 6
+
+    let counter = 0;
+
+    while (counter < 100) {
+      // creates a nice visual effect of the dice numbers changing
+      this.publicNumber1 = Math.floor(Math.random() * 6) + 1;
+      this.redNumber1 = Math.floor(Math.random() * 6) + 1;
+      this.yellowNumber1 = Math.floor(Math.random() * 6) + 1;
+      this.greenNumber1 = Math.floor(Math.random() * 6) + 1;
+      this.blueNumber1 = Math.floor(Math.random() * 6) + 1;
+      this.publicNumber2 = Math.floor(Math.random() * 6) + 1;
+      this.redNumber2 = Math.floor(Math.random() * 6) + 1;
+      this.yellowNumber2 = Math.floor(Math.random() * 6) + 1;
+      this.greenNumber2 = Math.floor(Math.random() * 6) + 1;
+      this.blueNumber2 = Math.floor(Math.random() * 6) + 1;
+
+      counter++;
+    }
+
+    this.publicNumber1 = Math.floor(Math.random() * 6) + 1;
+    this.redNumber1 = Math.floor(Math.random() * 6) + 1;
+    this.yellowNumber1 = Math.floor(Math.random() * 6) + 1;
+    this.greenNumber1 = Math.floor(Math.random() * 6) + 1;
+    this.blueNumber1 = Math.floor(Math.random() * 6) + 1;
+    this.publicNumber2 = Math.floor(Math.random() * 6) + 1;
+    this.redNumber2 = Math.floor(Math.random() * 6) + 1;
+    this.yellowNumber2 = Math.floor(Math.random() * 6) + 1;
+    this.greenNumber2 = Math.floor(Math.random() * 6) + 1;
+    this.blueNumber2 = Math.floor(Math.random() * 6) + 1;
   }
 }
--- functionalities/scripts.js
+++ functionalities/scripts.js
@@ -1,1 +1,5 @@
-const newRoll = document.querySelector('[]');
+import Dice from './dice';
+
+const newRoll = document.querySelector('[data-roll-dice]');
+
+newRoll.addEventListener('click', (e) => {});
