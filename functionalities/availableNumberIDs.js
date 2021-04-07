2068a2a143a6e84895bcb5f3b3270f34c7c79674
Ian Turner <ianturner88@gmail.com>
2021-04-07 18:10:27 -0400

    available number data attributes shuffled to separate js file

---
+++ functionalities/availableNumberIDs.js
@@ -0,0 +1,101 @@
+// the 6 dice
+const publicNumber1 = document.querySelector('[data-public-dice-1]');
+const publicNumber2 = document.querySelector('[data-public-dice-2]');
+const redDice = document.querySelector('[data-red-dice]');
+const yellowDice = document.querySelector('[data-yellow-dice]');
+const greenDice = document.querySelector('[data-green-dice]');
+const blueDice = document.querySelector('[data-blue-dice]');
+
+// player 1's 9 available numbers
+const player1AvailablePublicNumber = document.querySelector(
+  '[data-player-1-available-white]'
+);
+const player1AvailableRed1 = document.querySelector(
+  '[data-player-1-available-red-1]'
+);
+const player1AvailableRed2 = document.querySelector(
+  '[data-player-1-available-red-2]'
+);
+const player1AvailableYellow1 = document.querySelector(
+  '[data-player-1-available-yellow-1]'
+);
+const player1AvailableYellow2 = document.querySelector(
+  '[data-player-1-available-yellow-2]'
+);
+const player1AvailableGreen1 = document.querySelector(
+  '[data-player-1-available-green-1]'
+);
+const player1AvailableGreen2 = document.querySelector(
+  '[data-player-1-available-green-2]'
+);
+const player1AvailableBlue1 = document.querySelector(
+  '[data-player-1-available-blue-1]'
+);
+const player1AvailableBlue2 = document.querySelector(
+  '[data-player-1-available-blue-2]'
+);
+
+// player 2's 9 available numbers
+const player2AvailablePublicNumber = document.querySelector(
+  '[data-player-2-available-white]'
+);
+const player2AvailableRed1 = document.querySelector(
+  '[data-player-2-available-red-1]'
+);
+const player2AvailableRed2 = document.querySelector(
+  '[data-player-2-available-red-2]'
+);
+const player2AvailableYellow1 = document.querySelector(
+  '[data-player-2-available-yellow-1]'
+);
+const player2AvailableYellow2 = document.querySelector(
+  '[data-player-2-available-yellow-2]'
+);
+const player2AvailableGreen1 = document.querySelector(
+  '[data-player-2-available-green-1]'
+);
+const player2AvailableGreen2 = document.querySelector(
+  '[data-player-2-available-green-2]'
+);
+const player2AvailableBlue1 = document.querySelector(
+  '[data-player-2-available-blue-1]'
+);
+const player2AvailableBlue2 = document.querySelector(
+  '[data-player-2-available-blue-2]'
+);
+
+export {
+  // the dice numbers
+  publicNumber1,
+  publicNumber2,
+  redDice,
+  yellowDice,
+  greenDice,
+  blueDice,
+};
+
+export {
+  // the available numbers for player 1
+  player1AvailablePublicNumber,
+  player1AvailableRed1,
+  player1AvailableRed2,
+  player1AvailableYellow1,
+  player1AvailableYellow2,
+  player1AvailableGreen1,
+  player1AvailableGreen2,
+  player1AvailableBlue1,
+  player1AvailableBlue2,
+};
+
+export {
+  // the available numbers for player 2
+  player2AvailablePublicNumber,
+  player2AvailableRed1,
+  player2AvailableRed2,
+  player2AvailableYellow1,
+  player2AvailableYellow2,
+  player2AvailableGreen1,
+  player2AvailableGreen2,
+  player2AvailableBlue1,
+  player2AvailableBlue2,
+};
